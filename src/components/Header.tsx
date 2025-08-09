cbe6686f116079621faf4b77a987049478bd15fd
Mykyta Shyshov <workmailnikita@gmail.com>
2025-08-09 15:26:44 +0300

    [Header] Refactor + add the link to the home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <>
-      <nav className='p-4 bg-gray-800 text-white flex gap-4'>
-        <Link to='/'>Home</Link>
-        <Link to='/projects'>Projects</Link>
-        <Link to='/about'>About</Link>
+      <nav className='p-4 bg-gray-800 text-white flex justify-between items-center'>
+        <Link to='/' className='text-2xl font-bold'>
+          Sm.
+        </Link>
+        <div className='flex gap-12 text-lg'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/Resume'>Resume</Link>
+          <Link to='/projects'>Projects</Link>
+          <Link to='/github'>GitHub</Link>
+        </div>
       </nav>
     </>
   );

66d51214336634c8d35804cd47becf29645d6a11
Mykyta Shyshov <workmailnikita@gmail.com>
2025-08-16 16:28:16 +0300

    [Header] Fix HTML structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,38 +8,36 @@ const iconClass = 'h-4 w-4 md:h-5 md:w-5' as const;
 
 function Header() {
   return (
-    <>
-      <nav className='sticky top-0 z-nav p-4 flex justify-between items-center'>
-        <Link to='/' className='text-4xl text-stellar-gold font-bold'>
-          Sm.
+    <header className='sticky top-0 z-nav p-4 flex justify-between items-center'>
+      <Link to='/' className='text-4xl text-stellar-gold font-bold'>
+        Ms.
+      </Link>
+      <div className='flex items-center gap-12 text-lg'>
+        <Link to='/' className='nav-link'>
+          <HomeIcon className={iconClass} title='Home' />
+          Home
         </Link>
-        <div className='flex items-center gap-12 text-lg'>
-          <Link to='/' className='nav-link'>
-            <HomeIcon className={iconClass} title='Home' />
-            Home
-          </Link>
-          <Link to='/about' className='nav-link'>
-            <UserIcon className={iconClass} title='About' />
-            About
-          </Link>
-          <Link to='/resume' className='nav-link'>
-            <ResumeIcon className={iconClass} title='Resume' />
-            Resume
-          </Link>
-          <Link to='/projects' className='nav-link'>
-            <ProjectsIcon className={iconClass} title='Projects' />
-            Projects
-          </Link>
-          <Link
-            to={GITHUB_URL}
-            className='text-galactic-gray hover:text-nebula-pink hover:scale-115 duration-300'
-            target='_blank'
-          >
-            <GitHubIcon className={iconClass} title='GitHub' />
-          </Link>
-        </div>
-      </nav>
-    </>
+        <Link to='/about' className='nav-link'>
+          <UserIcon className={iconClass} title='About' />
+          About
+        </Link>
+        <Link to='/resume' className='nav-link'>
+          <ResumeIcon className={iconClass} title='Resume' />
+          Resume
+        </Link>
+        <Link to='/projects' className='nav-link'>
+          <ProjectsIcon className={iconClass} title='Projects' />
+          Projects
+        </Link>
+        <Link
+          to={GITHUB_URL}
+          className='text-galactic-gray hover:text-nebula-pink hover:scale-115 duration-300'
+          target='_blank'
+        >
+          <GitHubIcon className={iconClass} title='GitHub' />
+        </Link>
+      </div>
+    </header>
   );
 }
 

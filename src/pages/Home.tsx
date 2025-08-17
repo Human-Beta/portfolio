47d01ce6fe13dd9b9577b3facbfbbced4b1456d2
Mykyta Shyshov <workmailnikita@gmail.com>
2025-08-17 23:10:25 +0300

    [Home] Add dynamic typewriter effect to Home page
    Integrated `react-simple-typewriter` to enhance the Home page with a typewriter animation in the "Software Developer" section. Updated dependencies in `package.json` and `package-lock.json` accordingly.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Typewriter } from 'react-simple-typewriter';
+
 const Home = () => {
   return (
     <div className='flex flex-1 items-center justify-between px-25'>
-      <div className='flex flex-col gap-15 p-4 text-lunar-gray'>
-        <div className='flex flex-col gap-5'>
+      <div className='flex flex-col gap-13 p-4 text-lunar-gray'>
+        <div className='flex flex-col gap-4'>
           <h1 className='text-3xl'>
             Hi There!
             <span className='inline-block origin-[70%_70%] animate-wave' role='img' aria-labelledby='wave'>
@@ -10,10 +12,18 @@ const Home = () => {
             </span>
           </h1>
           <h1 className='text-4xl'>
-            I'M <strong className='text-stellar-gold'>MYKYTA SHYSHOV</strong>
+            I'm <strong className='text-stellar-gold'>Mykyta Shyshov</strong>
           </h1>
         </div>
-        <h1 className='text-2xl text-stellar-gold'>Software Developer</h1>
+        <h1 className='text-4xl text-stellar-gold'>
+          <Typewriter
+            words={['Software Developer', 'Fullstack', 'Freelancer']}
+            loop
+            cursor
+            typeSpeed={100}
+            delaySpeed={3000}
+          />
+        </h1>
       </div>
       <img
         src='https://cdn.shopify.com/s/files/1/0565/8021/0861/files/steptodown_3.png?v=1707392483'

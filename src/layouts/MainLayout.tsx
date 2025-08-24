0ff38c20c999a8c40bd53e8187843f6313bb69b2
Mykyta Shyshov <workmailnikita@gmail.com>
2025-08-24 20:58:23 +0300

    [Header] Update styles and refactor layout container classes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,12 +4,14 @@ import BackgroundStars from '@/components/BackgroundStars';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
+const containerClass = 'container mx-auto px-4 md:px-8 lg:px-30';
+
 function MainLayout() {
   return (
     <div className='bg-deep-space'>
       <BackgroundStars />
-      <div className='container mx-auto px-4 md:px-8 lg:px-30 flex min-h-screen flex-col'>
-        <Header />
+      <Header className={containerClass} />
+      <div className={`${containerClass} flex min-h-screen flex-col`}>
         <main className='flex flex-1 py-8'>
           <Outlet />
         </main>

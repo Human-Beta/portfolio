1c0411a1fb179003ec387b6e0a53471be3c10320
Mykyta Shyshov <workmailnikita@gmail.com>
2025-08-31 22:08:41 +0300

    [Resume] Add DownloadIcon to DownloadButton component

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import { DownloadIcon } from './icons/Icons';
+
 interface Props {
   itemName: string;
 }
@@ -10,8 +12,9 @@ const DownloadButton = ({ itemName }: Props) => {
       to='/resume.pdf'
       target='_blank'
       rel='noopener noreferrer'
-      className='px-10 py-2 bg-stellar-gold/70 text-white rounded-lg hover:bg-stellar-gold/80 duration-300'
+      className='px-10 py-2 bg-stellar-gold/70 text-white rounded-lg hover:bg-stellar-gold/80 duration-300 flex items-center gap-2'
     >
+      <DownloadIcon className='w-4 h-4' title='Download' />
       Download {itemName}
     </Link>
   );
